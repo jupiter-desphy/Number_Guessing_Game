ec18e88426472013ed63d17aaf9aa08b8c3035ad
Jupiter Desphy <jupiterdesphy7@gmail.com>
2022-08-24 09:27:27 -0700

    adding ben's example

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-let rand = 7;
 
 let min = 1;
-let max = 10;
-let favNumber = 7 
-let input = prompt(`What is my favorite number between ${min} and ${max}?`);
-input = parseFloat (input);
+let max = 100;
+let rand = Math.floor(Math.random() * max) +1;
+let isPlaying = true
 
-if (input < favNumber) {
-  alert ("too low");
-} else if (input > favNumber) {
-  alert ("too high");
-} else if (input == favNumber) {
-  alert ("Congratulations!!!");
-}  else {
-  alert ("Please enter a valid number");
-  
+while (isPlaying) {
+    turn()
+}
+    function turn() {
+    let input = Number.parseInt(prompt(`What is my favorite number between ${min} and ${max}?`)); 
+    if (input < rand) {
+        alert ("Too low. Try again.");
+        } else if (input > rand) {
+        alert ("Too high. Try again.");
+        } else if (input == rand) {
+        alert ("Congratulations!!!");
+        isPlaying = false;
+        }  else {
+        alert (`Please enter a valid number between ${min} and ${max}.`);
+        
+    }
 }
